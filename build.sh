cf82d7ac1213130ddb0358ed1a901bb8f86f4ebb
Tom Klun <tklun@arc-chi-thomas-klun.local>
2011-05-04 10:59:59 -0500

    Finished up the build.sh script to add maven commands

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "Reading config..."
 source mvbuild.config
 # Source file will include the following path names
 	# tomcat_home - This is the main directory for all Tomcat install
@@ -10,31 +11,40 @@ param2=$2
 if [ "$param1" = "-hot" ]; then
 	# If the first parameter is -hot, script will hot deploy to the second parameter
 	echo "Hot deploying..."
-  echo "Deploy target: $tomcat_home/$param2"
+	project_name=$param2
+	echo "Deploy target: $tomcat_home/$project_name"
 	echo "Run hot deploy code"
+	command="mvn -Dtomcat.home=$tomcat_home/$project_name -Dmaven.test.skip=true clean install"
 	shift 2
 else
 	if [ "$param1" = "-clean" ]; then
 		# If the first parameter is -clean, script will run a clean install to the second parameter
     #echo "Run clean install HAS CLEAN PARAM"
 		echo "Building clean install..."
-	  echo "Deploy target: $tomcat_home/$param2"
+		project_name=$param2
+		echo "Deploy target: $tomcat_home/$param2"
+		command="mvn -Dtomcat.home=$tomcat_home/$project_name -Dmaven.test.skip=true clean install"
 		shift 2
 	else
 		# If the first parameter is not -clean or -hot, script will default to a clean install to the first parameter
   	#echo "Run clean install NO PARAM"
 		echo "Building clean install..."
-	  echo "Deploy target: $tomcat_home/$param1"
+		project_name=$param1
+		echo "Deploy target: $tomcat_home/$project_name"
+		command="mvn -Dtomcat.home=$tomcat_home/$project_name -Dmaven.test.skip=true clean install"
 		shift
   fi
 fi
 
+echo "Executing on project: $project_name"
+
 # Loop through the remaining parameters, changing directories through the workspace and build all necessary poms
 until [ -z "$1" ]  # Until all parameters used up . . .
 do
-  echo "Change directory and execute again"
+	echo "Project name: $project_name"
 	echo "Tomcat directory: $tomcat_home"
-  # cd $workspace_home/$1
-	echo $workspace_home/${1}
+	echo "Building project: $workspace_home/$project_name/$1"
+	cd $workspace_home/$project_name/$1
+	$command
 	shift
 done
