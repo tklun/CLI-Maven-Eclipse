acf1c99f0c8bfde66921247c4ebef6cad27875f5
Tom Klun <tom@tomklun.com>
2011-05-05 09:40:36 -0500

    Added updates to the Tomcat script

--- tcat.sh
+++ tcat.sh
@@ -4,15 +4,17 @@ source mvbuild.config
 	# tomcat_home - This is the main directory for all Tomcat install
 	# workspace_home - This is the main directory containing Workspaces for all projects
 
-action = $1
-instance = $2
+action=$1
+instance=$2
 
 cd $tomcat_home/$instance
 
 if [ "$action" = "start" ]; then
+	echo "Starting Tomcat in $tomcat_home/$instance ..."
 	bin/startup.sh
 else
 	if [ "$action" = "stop" ]; then
+		echo "Shutting down Tomcat in $tomcat_home/$instance ..."
 		bin/shutdown.sh
 	else
 		echo "Please either start or stop Tomcat..."
